ef2ccdd4cb398d3a0289ca71c97a7f05c98bfd30
DVectorprime <vectorprime2024@gmail.com>
2025-03-25 10:59:43 +0100

    Job done

--- vite.config.js
+++ vite.config.js
@@ -6,36 +6,36 @@ const manifestForPlugIn = {
   registerType:'prompt',
   includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
   manifest:{
-    name:"Vector Prime Notes App",
-    short_name:"VP Notes",
-    description:"Your note app that is accessible everywhere you go.",
+    name:"React-vite-app",
+    short_name:"react-vite-app",
+    description:"I am a simple vite app",
     icons:[{
-      src: '/logo-192x192.png',
+      src: '/android-chrome-192x192.png',
       sizes:'192x192',
       type:'image/png',
       purpose:'favicon'
     },
     {
-      src:'/logo-512x512.png',
+      src:'/android-chrome-512x512.png',
       sizes:'512x512',
       type:'image/png',
       purpose:'favicon'
     },
     {
-      src: '/logo180x180.png',
+      src: '/apple-touch-icon.png',
       sizes:'180x180',
       type:'image/png',
       purpose:'apple touch icon',
     },
     {
-      src: '/logo-512x512.png',
+      src: '/maskable_icon.png',
       sizes:'512x512',
       type:'image/png',
       purpose:'any maskable',
     }
   ],
-  theme_color:'#000064',
-  background_color:'#ffffff',
+  theme_color:'#000054',
+  background_color:'#fff',
   display:"standalone",
   scope:'/',
   start_url:"/",
