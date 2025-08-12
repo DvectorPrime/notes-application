fdf207ce052a51dcd07612dd9af826e829669d0b
DvectorPrime <vectorprime2024@gmail.com>
2025-08-12 02:03:31 +0100

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,81 +1,81 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {app, db} from '../firebase/firebaseConfig'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { doc, setDoc, getDocs, collection } from "firebase/firestore"; 
-import googleIcon from '../assets/google.png';
+// import {app, db} from '../firebase/firebaseConfig'
+// import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import { doc, setDoc, getDocs, collection } from "firebase/firestore"; 
+// import googleIcon from '../assets/google.png';
 
-import { useAppData } from '../context/CurrentUserContext';
-import ToggleTheme from './singleComponents/toggleTheme';
-import welcomeMessage from '../assets/welcome_data';
-import "./login.css"
+// import { useAppData } from '../context/CurrentUserContext';
+// import ToggleTheme from './singleComponents/toggleTheme';
+// import welcomeMessage from '../assets/welcome_data';
+// import "./login.css"
 
-const Login = () => {
+// const Login = () => {
     
-    const {currentUser, setCurrentUser, theme} = useAppData()
+//     const {currentUser, setCurrentUser, theme} = useAppData()
     
-    const provider = new GoogleAuthProvider();
+//     const provider = new GoogleAuthProvider();
     
-    const auth = getAuth(app);
+//     const auth = getAuth(app);
 
-    const navigate = useNavigate()
+//     const navigate = useNavigate()
 
-    //Authenticates User
-    async function handleLogin() {
-        try {
-            const result = await signInWithPopup(auth, provider);
+//     //Authenticates User
+//     async function handleLogin() {
+//         try {
+//             const result = await signInWithPopup(auth, provider);
 
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
+//             // This gives you a Google Access Token. You can use it to access the Google API.
+//             const credential = GoogleAuthProvider.credentialFromResult(result);
+//             const token = credential.accessToken;
 
-            // The signed-in user info.
-            const user = result.user;
-            console.log(user)
-            setCurrentUser(user)    
-            localStorage.setItem("currentUser", JSON.stringify(user))
-        } catch (error) {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            alert(`${errorCode} : ${errorMessage}`)
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-        }
-    }
+//             // The signed-in user info.
+//             const user = result.user;
+//             console.log(user)
+//             setCurrentUser(user)    
+//             localStorage.setItem("currentUser", JSON.stringify(user))
+//         } catch (error) {
+//             // Handle Errors here.
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+//             alert(`${errorCode} : ${errorMessage}`)
+//             // The email of the user's account used.
+//             const email = error.customData.email;
+//             // The AuthCredential type that was used.
+//             const credential = GoogleAuthProvider.credentialFromError(error);
+//         }
+//     }
 
-    async function getUserInformation() {
+//     async function getUserInformation() {
 
-        //Holds info on whether the user has a collection
-        const noteSnapShots = await getDocs(collection(db, currentUser.uid))
+//         //Holds info on whether the user has a collection
+//         const noteSnapShots = await getDocs(collection(db, currentUser.uid))
 
-        //Holds info for creating new collection if the user has none
-        const notesRef = doc(db, currentUser.uid, "Welcome To Notes App")
+//         //Holds info for creating new collection if the user has none
+//         const notesRef = doc(db, currentUser.uid, "Welcome To Notes App")
         
-        const data = {
-            id: "Welcome To Notes App",
-            heading: "A guide on how to use the notes app",
-            body: welcomeMessage,
-            date: Date.now(),
-            category: "Welcome"
-         }
+//         const data = {
+//             id: "Welcome To Notes App",
+//             heading: "A guide on how to use the notes app",
+//             body: welcomeMessage,
+//             date: Date.now(),
+//             category: "Welcome"
+//          }
         
-        if (noteSnapShots.length){
-            navigate("/")
-        } else {
-            await setDoc(notesRef, data) 
-            navigate("/")
-        }
-    }
+//         if (noteSnapShots.length){
+//             navigate("/")
+//         } else {
+//             await setDoc(notesRef, data) 
+//             navigate("/")
+//         }
+//     }
     
-    //Call getUserInformation to Authenticate User if there is no user authenticated
-    useEffect(() => {
-        if (currentUser != ""){
-            getUserInformation()
-        }
-    }, [currentUser])
+//     //Call getUserInformation to Authenticate User if there is no user authenticated
+//     useEffect(() => {
+//         if (currentUser != ""){
+//             getUserInformation()
+//         }
+//     }, [currentUser])
 
     return (
         <div className={`login-page ${theme === 'dark' && 'dark'}`}>
